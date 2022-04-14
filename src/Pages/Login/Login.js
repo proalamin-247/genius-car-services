128aa7475ef6aeb551911f80603f0504443e2327
proalamin-247 <pro.alamin247@gmail.com>
2022-04-14 21:37:15 +0600

    login form added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate = useNavigate();
+
+    const handaleSubmit = event=>{
+        event.preventDefault();
+        const email= emailRef.current.value;
+        const password= passwordRef.current.value;
+
+    }
+    const navigateRagister= event=>{
+        navigate('/register')
+    }
+
     return (
-        <div>
-            
+        <div className='container w-25 mx-auto mt-5'>
+            <h2 className='text-primary text-center'>Please Login...</h2>
+            <Form onSubmit={handaleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required/>
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+                <p>Have not account? <span className='text-danger' onClick={navigateRagister}>Please Register</span></p>
+            </Form>
         </div>
     );
 };
