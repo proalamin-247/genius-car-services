a319471c7b24b62a245018518119ff0d066695af
proalamin-247 <pro.alamin247@gmail.com>
2022-04-16 02:47:40 +0600

    github login added

--- src/Pages/Login/Social Login/SocialLogin.js
+++ src/Pages/Login/Social Login/SocialLogin.js
@@ -3,29 +3,30 @@ import googleLogo from '../../../images/social/googleLogo.png';
 import { FcGoogle } from 'react-icons/fc';
 import { BsFacebook } from 'react-icons/bs';
 import { SiGithub } from 'react-icons/si';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 
 const SocialLogin = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
-    const navigate= useNavigate();
+    const [signInWithGithub, user1, loading1, error1] = useSignInWithGithub(auth);
+    const navigate = useNavigate();
     const location = useLocation();
     let from = location.state?.from?.pathname || "/";
     let errorElement;
 
-    if (error) {
-        errorElement= (
+    if (error || error1) {
+        errorElement = (
             <div>
-                <p className='text-danger'>login failed</p>
+                <p className='text-danger'>Error: {error?.message} {error1?.message}</p>
             </div>
         );
     }
-    if (loading) {
+    if (loading || loading1) {
         return <p>Loading...</p>;
     }
-    if (user) {
+    if (user || user1) {
         // navigate('/home')
         navigate(from, { replace: true });
     }
@@ -33,7 +34,7 @@ const SocialLogin = () => {
     return (
         <div>
             <div className='d-flex align-items-center justify-content-center'>
-                <div style={{height: '1px'}} className="bg-primary w-25"></div>
+                <div style={{ height: '1px' }} className="bg-primary w-25"></div>
                 <p className='px-2 mt-2'>or</p>
                 <div style={{ height: '1px' }} className="bg-primary w-25"></div>
             </div>
@@ -47,7 +48,7 @@ const SocialLogin = () => {
                     {/* <img src={googleLogo} alt="" /> */}
                     <span><BsFacebook /></span> <span className='px-1 mt-2'>Continue with  Facebook </span>
                 </button>
-                <button className='btn btn-white w-75 border border-info d-block mx-auto mt-2'>
+                <button onClick={() => signInWithGithub()} className='btn btn-white w-75 border border-info d-block mx-auto mt-2'>
                     {/* <img src={googleLogo} alt="" /> */}
                     <span><SiGithub /></span> <span className='px-1 mt-2'>Continue with  Github </span>
                 </button>
